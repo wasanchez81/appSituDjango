8e48ff0666539b2002900f4e39d6c4d269a97fd6
Wilson Sanchez <wasanchez@live.com>
2022-10-25 20:29:58 -0500

    Subir proyecto situ update9

--- ProyectoSITU/settings.py
+++ ProyectoSITU/settings.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,6 +121,6 @@ STATICFILES_DIRS = [
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
