60514366ce35c0ca49572bbbfb9fe7b84d8889da
Wilson Sanchez <wasanchez@live.com>
2022-10-25 23:41:05 -0500

    Subir proyecto situ update15

--- ProyectoSITU/settings.py
+++ ProyectoSITU/settings.py
@@ -122,6 +122,7 @@ STATICFILES_DIRS = [
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
