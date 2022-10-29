fc7bae6ad7dea0e4894f0effe2b8fd292c9c3527
Wilson Sanchez <wasanchez@live.com>
2022-10-29 16:31:13 -0500

    Subir proyecto situ update18

--- ProyectoSITU/settings.py
+++ ProyectoSITU/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from genericpath import exists
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
